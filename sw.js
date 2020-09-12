15701076f01c6ca9d3e6863b32d29a730598d5ac
Anyanime <bensonanyanime@gmail.com>
2020-09-12 22:19:26 +0100

    fix bug

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-if ('serviceWorker' in navigator) {
-    console.log('Yes!!!!')
+
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
-            navigator.serviceWorker.register('./sw.js');
+        console.log("yes")
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('./sw1.js')
+                .then((reg) => {
+                    // registration worked
+                    console.log('Registration succeeded. Scope is ' + reg.scope);
+                }).catch((error) => {
+                    // registration failed
+                    console.log('Registration failed with ' + error);
+                });
         });
     }
-}
-
