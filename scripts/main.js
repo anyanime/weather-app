856c5cab0cc5872b731aba85c9e6de4d9193f401
Anyanime <bensonanyanime@gmail.com>
2020-09-12 13:31:01 +0100

    manipulate dom

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,32 @@
 
-
- apiKey = "671ac19158660d3002a5c97bffb59b1b",
  search = document.getElementById("button"),
- city = document.getElementById("input"),
- condition = document.getElementById("condition").innerHTML;
- date = document.getElementById("date").innerHTML;
- temp = document.getElementById("temp").innerHTML;
- place = document.getElementById("place").innerHTML;
 
-    search.addEventListener('click', () => {
-        date = new Date();
-        console.log(city.value);
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}`)
+    search.addEventListener('click', (reg) => {
+       let city = document.getElementById("input");
+       let apiKey = "671ac19158660d3002a5c97bffb59b1b";
+        //fectch data from the API
+       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                result = data;
+
+                // get html data
+                condition = document.getElementById("condition").innerHTML;
+                date = document.getElementById("date").innerHTML;
+                temp = document.getElementById("temp").innerHTML;
+                place = document.getElementById("place").innerHTML;
+                console.log(result)
+
+                //assign results to html data
+                
+                document.getElementById("place").innerHTML = result.name
+                document.getElementById("date").innerHTML = new Date(result.dt*1000).toDateString();
+                document.getElementsByTagName('img').item(0).src = "http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x" +  ".png"
+                document.getElementById("condition").innerHTML = result.weather[0].description;
+                document.getElementById("temp").innerHTML = Math.round(result.main.temp) + '&deg' + 'C'
+                console.log(result.name, result.dt*1000, result.main.temp, result.weather[0].main, result.main.pressure, result.main.humidity,  result.weather[0].description)
                 })
             .catch(err => console.log(err));
 })
 
-// fetch( 
-//     `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&appid=${apiKey}`
-// ).then(res => res.json())
-//     .then(body => console.log(body.weather))
-//     .catch(err => console.log(err));
 
-
-// search.addEventListener('click', () => { 
-//     console.log(city.value); 
-// });
-
-// fetch( 
-//     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-// ).then(res => res.json())
-//     .then(body => console.log(body.weather))
-//     .catch(err => console.log(err))
-
-// console.log(city)
